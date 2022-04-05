2597f9f045e6bbed39e3cf3082b46d5e8ac7b1da
itsajay2195 <ajaykumar.95@yahoo.com>
2022-04-05 22:20:49 +0530

    desigining of the bottomtab component and incorporated the same on the respective screens

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
 import React from 'react'
+import BottomTabs from '../components/common/BottomTab'
 
 const Home = () => {
   return (
-    <View style={{backgroundColor:"#01D167"}}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{flex:1,backgroundColor:"white"}}>
+      <View style={{flex:1}}>
+          <Text> Debit section</Text>
+      </View>
+
+      <View style={{flex:1}}>
+          <Text> Sliding Up Panel</Text>
+      </View>
+
+      <View style={{flex:1, justifyContent:'flex-end'}}>
+          <BottomTabs/>
+      </View>
+    </SafeAreaView>
   )
 }
 
