631092e8e47015f45ffb02d70f63f1371d76ad20
itsajay2195 <ajaykumar.95@yahoo.com>
2022-04-05 22:48:08 +0530

    desigining of the header  component and incorporated the same on the respective screens

--- navigation.js
+++ navigation.js
@@ -6,7 +6,6 @@ import WeeklyLimit from './src/screens/WeeklyLimit'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import {icons} from './src/constants'
 import { LOGO } from './src/constants';
 // import { Provider } from 'react-redux'
 // import {store} from './src/redux/store'
@@ -18,26 +17,17 @@ export default function RootNavigation() {
     const Stack = createStackNavigator();
 
     const screenOptions = {
-        headerShown: true,
+        headerShown: false,
         headerTransparent: true,
-
+        headerBackTitleVisible: false
     }
 
     return (
         // <Provider store={store}>
             <NavigationContainer>
                 <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-                    <Stack.Screen name='Home' component={Home} options={{
-                        title: '',
-                        headerBackTitleVisible: false,
-                        headerRight: () => <Image source={icons.logo} style={{ height: 20, width: 20, right: 30 }} />
-                    }}></Stack.Screen>
-                    <Stack.Screen name='Limit' component={WeeklyLimit} options={{
-                        title: '',
-                        headerBackImage: () => <MaterialCommunityIcons name="chevron-left" size={40} color="#fff" />,
-                        headerBackTitleVisible: false,
-                        headerRight: () => <Image source={LOGO} style={{ height: 20, width: 20, right: 30 }} />
-                    }}></Stack.Screen>
+                    <Stack.Screen name='Home' component={Home} />
+                    <Stack.Screen name='Limit' component={WeeklyLimit}/>
                 </Stack.Navigator>
             </NavigationContainer>
         // </Provider>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
 import React from 'react'
 import BottomTabs from '../components/common/BottomTab'
+import Header from '../components/common/Header'
 
 const Home = () => {
   return (
     <SafeAreaView style={{flex:1,backgroundColor:"white"}}>
+      <View>
+            <Header showBack={false}></Header>
+      </View>
+
       <View style={{flex:1}}>
           <Text> Debit section</Text>
       </View>
